62412836e24905621ff7e1fec1f21697fb821fcd
Vincenzo Rojas Carrera <vincenzo.rojas.carrera@gmail.com>
2025-11-25 19:43:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import json
 from peewee import * # type: ignore
 from conexion import inicializar_base, db
 from pprint import pprint
-from Modelos.Like_Model import Like
-from Modelos.Post_Model import Post
-from Modelos.Usuario_Model import Usuario
+from Modelos.X_Model import X
 
 def main():
-    tablas = [Like, Post, Usuario]
+    #inicializar tablas
+    tablas = [Tabla1, Tabla2...]
 
     #inicializar_base
     inicializar_base(tablas, True)
 
 
 if __name__ == "__main__":
+
     main()
